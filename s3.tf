935a05218659576979c18f7b3a06bef5d3068d5b
Eric-Kay <126447235+Eric-Kay@users.noreply.github.com>
2024-03-28 15:32:46 +0000

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -61,12 +61,3 @@ resource "aws_s3_bucket_website_configuration" "website" {
   }
   depends_on = [ aws_s3_bucket_acl.example.id ]
 }
-resource "aws_s3_bucket" "example_bucket" {
-  bucket = var.bucket_name
-
-  versioning {
-    enabled = true
-  }
-
-  acl = var.acl
-}
