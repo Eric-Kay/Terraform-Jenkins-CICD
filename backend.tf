cb0ecbc5a66e0a5dc045fdb94c29d09c514a11ab
Eric-Kay <126447235+Eric-Kay@users.noreply.github.com>
2024-03-28 13:52:00 +0000

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "eric-devops-jenkins"
     key            = "my-terraform-environment/main"
-    region         = "ap-south-1"
+    region         = "eu-west-2"
     dynamodb_table = "eric-devops-jenkins-dynamo-db"
   }
 }
