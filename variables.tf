93b5f6a0ddeb307060c2d05a648885162b51eacb
Eric-Kay <126447235+Eric-Kay@users.noreply.github.com>
2024-03-28 16:55:40 +0000

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -17,7 +17,7 @@ variable "ami_id" {
 variable "bucket_name" {
   description = "The name of the S3 bucket to create"
   type        = string
-  default     = "eric-devops-terra-buck"
+  default     = "eric-devops-terra-buck-2"
 }
 variable "acl" {
   description = "The ACL (Access Control List) for the S3 bucket"
