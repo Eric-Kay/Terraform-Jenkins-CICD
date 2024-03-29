d9ddcc33e1de2fc05cd1a3b1729fc9892959e101
Eric-Kay <126447235+Eric-Kay@users.noreply.github.com>
2024-03-29 23:12:48 +0000

    Delete backend.tf

--- backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "eric-devops-jenkins"
-    key            = "my-terraform-environment/main"
-    region         = "eu-west-2"
-    dynamodb_table = "eric-devops-jenkins-dynamo-db"
-  }
-}
