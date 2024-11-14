ca37fbd08fe6ddde8615db9209ae3ed99dfeec66
Eric-Kay <126447235+Eric-Kay@users.noreply.github.com>
2024-11-14 06:19:58 +0000

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "jenkins-mama"
+    bucket         = "jenkins-mama-2024"
     key            = "my-terraform-environment/main"
     region         = "us-east-1"
     dynamodb_table = "eric-devops-jenkins-dynamo-db"
