fa9da4e003e2f40cd50ba1e9e5549c539285f9ef
Eric-Kay <126447235+Eric-Kay@users.noreply.github.com>
2024-11-14 05:49:12 +0000

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -17,5 +17,5 @@ variable "ami_id" {
 variable "bucket_name" {
   description = "The name of the S3 bucket to create"
   type        = string
-  default     = "terra-mama-2024"
+  default     = "terra-mama-2024demo"
 }
